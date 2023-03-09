6b5731376ffa5d93be6012839fe842ea7bc6d131
victorojedeji <ojedejivictordaniel@gmail.com>
2023-03-09 18:16:20 +0100

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import React from 'react'
+import {
+  Route, 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider
+} from 'react-router-dom'
+
+// Pages
+
+import {
+  SignUp,
+  Login, 
+  About,
+  Home,
+  Contact,
+  NotFound,
+  Pricing,
+  Categories,
+  MyAccount
+  } from './Page'
 
 
-function App() {
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
+        <Route path="about" element={<About />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="my-account" element={<MyAccount />} />
+      </Route>
+  )
+);
+const App = () => {
   return (
-    <div className="App">
-      
+    <div>
+      <RouterProvider router={router}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
