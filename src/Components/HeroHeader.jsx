796ff0ad9439b6258d2166472a88808acad8ed5f
victorojedeji <ojedejivictordaniel@gmail.com>
2023-03-14 14:13:13 +0100

    hero form

--- src/Components/HeroHeader.jsx
+++ src/Components/HeroHeader.jsx
@@ -6,6 +6,14 @@ const HeroHeader = () => {
   return (
     <main className={styles.hero_container}>
       <img src={heroHeadeImg} alt='hero-header' className={styles.hero_img} />
+
+      <div className={styles.hero_content}>
+        <h1 className={styles.hero_text}>Read reviews. Write reviews. <br/>Find companies you can <span className={styles.colored}>trust.</span></h1>  
+        <div className={styles.form_wrapper}>
+          <input placeholder='Company or category' className={styles.form_input}/>
+          <button className={styles.form_btn}>search</button>
+        </div>
+      </div>
     </main>
   )
 }
